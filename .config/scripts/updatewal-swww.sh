3e8bc09151da4c6579462c90f4a44b379b1f69d3
notsungod <fyodor.alyokhin@gmail.com>
2024-04-18 02:08:28 +0200

    changes

---
+++ .config/scripts/updatewal-swww.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wal -q -i "$HOME/Pictures/wallpapers"
+source "$HOME/.cache/wal/colors.sh"
+cp ~/.cache/wal/colors-waybar.css ~/.config/waybar
+swww img $wallpaper --transition-step 20 --transition-fps=20 
+~/.config/waybar/reload.sh
